cd6db940d3e15100e756bcbb4459c49b1aa700c0
Tatsuki Hayakawa <tat.hayakawa@gmail.com>
2023-07-22 14:15:37 +0900

    wip: Admin UserController

---
+++ tests/Feature/Http/Controllers/Admin/UserControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Admin;
+
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
