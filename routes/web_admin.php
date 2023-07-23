4c9a452a8256b2f987ffaeb77ee59b3357a25d52
Tatsuki Hayakawa <tat.hayakawa@gmail.com>
2023-07-23 21:31:32 +0900

    fix: route

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\StoryController;
 use App\Http\Controllers\Admin\TopController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/users', UserController::class);
+Route::resource('/user', UserController::class);
+Route::resource('/story', StoryController::class)->only(['index', 'show', 'edit', 'update']);
 Route::get('/', TopController::class)->name('top.index');
