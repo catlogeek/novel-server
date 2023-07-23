34070470bdf0baa557137f3922d296dbb394d6c7
Tatsuki Hayakawa <tat.hayakawa@gmail.com>
2023-07-24 08:13:35 +0900

    feat: レビュー管理

--- resources/views/admin/review/_partials/_show.blade.php
+++ resources/views/admin/review/_partials/_show.blade.php
@@ -1,54 +1,39 @@
-<div class="row">
-  <div class="col-md-6">
-    <x-form-group class="mb-3">
-      <x-form-input label="ID" name="id" value="{{ $story->id }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="ID" name="id" value="{{ $review->id }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="タイトル" name="title" value="{{ $story->title }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="レビュアー" name="user.name" value="{{ $review->user->name }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="ジャンル" name="Genre" value="{{ $story->Genre->display() }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="作品" name="story.title" value="{{ $review->story->title }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="キャッチフレーズ" name="catchphrase" value="{{ $story->catchphrase }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="タイトル" name="title" value="{{ $review->title }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-textarea label="紹介文" name="introduction" default="{{ $story->introduction }}" rows="10" style="min-height: fit-content;" readonly>
-      </x-form-textarea>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-textarea label="本文" name="body" default="{{ $review->body }}" rows="50" style="min-height: fit-content;" readonly>
+  </x-form-textarea>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="ステータス" name="Status" value="{{ $story->Status->display() }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="ステータス" name="Status" value="{{ $review->Status->display() }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="登録日" name="created_at" value="{{ $story->created_at }}" readonly>
-      </x-form-input>
-    </x-form-group>
+<x-form-group class="mb-3">
+  <x-form-input label="登録日" name="created_at" value="{{ $review->created_at }}" readonly>
+  </x-form-input>
+</x-form-group>
 
-    <x-form-group class="mb-3">
-      <x-form-input label="最終更新日" name="updated_at" value="{{ $story->updated_at }}" readonly>
-      </x-form-input>
-    </x-form-group>
-  </div>
-
-  <div class="col-md-6">
-    <x-form-label label="タグ" />
-    <div class="d-flex flex-wrap gap-2">
-      @foreach ($story->storyTag as $item)
-        <x-form-input class="w-auto" name="storyTag[]" value="{{ $item->text }}" readonly>
-        </x-form-input>
-      @endforeach
-    </div>
-  </div>
-
-</div>
+<x-form-group class="mb-3">
+  <x-form-input label="最終更新日" name="updated_at" value="{{ $review->updated_at }}" readonly>
+  </x-form-input>
+</x-form-group>
